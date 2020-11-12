53aa7213e31d99bdb2dffd8e3d88b00e795e45c4
saad28 <74276621+saad28@users.noreply.github.com>
2020-11-12 14:36:45 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score
+var survivalTime=0;
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  
+
+  
+}
+
+
+function draw() {
+stroke("white");
+textSize(20);
+fill("white");
+text("score:"+ score,500,50);
+  
+  
+stroke("black")
+textSize(20);
+fill("black");
+survivalTime=matgh.ceil(frameCount/frameRate())
+text("survivalTime: " + survivalTime, 100,50)
+}
+// creating monkey
+monkey=createSprite(80,315,20,20);
+monkey.addAnimation("moving",monkey_running);
+monkey.scale=0.1
+
+ground = createSprite(400,350,900,10);
+ground.velocityX=-4;
+ground.x=ground.width/2;
+console.log(ground.x)
+
+
+
+
+
